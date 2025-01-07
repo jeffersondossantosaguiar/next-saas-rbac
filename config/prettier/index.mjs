80b1a6a2af2d9c7cd96552d0fcec1ca9860f99aa
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-01-07 14:41:01 -0300

    chore: create account service

--- config/prettier/index.mjs
+++ config/prettier/index.mjs
@@ -1,6 +1,6 @@
 /** @typedef {import('prettier').Config} PrettierConfig */
 
-/** @type { PrettierConfig }  */
+/** @type { PrettierConfig } */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
   printWidth: 80,
