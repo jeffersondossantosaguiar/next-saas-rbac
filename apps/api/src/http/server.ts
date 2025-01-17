72ff068fed54e87bd4e71d1e64ada1260ad97685
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-01-17 14:56:38 -0300

    chore: add github oauth

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -10,6 +10,7 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { errorHandler } from './error-handler'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
@@ -47,6 +48,7 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
