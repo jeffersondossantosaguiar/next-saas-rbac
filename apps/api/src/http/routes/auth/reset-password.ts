b3971c589787f2222c11c03fcd11b59e07622d04
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-01-29 16:21:32 -0300

    chore: create cascades

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -23,21 +23,33 @@ export async function resetPassword(app: FastifyInstance) {
     },
     async (request, reply) => {
       const { code, password } = request.body
+
       const tokenFromCode = await prisma.token.findUnique({
         where: { id: code },
       })
+
       if (!tokenFromCode) {
         throw new UnauthorizedError()
       }
+
       const passwordHash = await hash(password, 6)
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
+
       return reply.status(204).send()
     }
   )
