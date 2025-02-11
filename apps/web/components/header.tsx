7f8e90b569d25a7929bf6d0c60b8e86b361ebfb0
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-02-11 18:41:36 -0300

    chore: organization switcher

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,5 +1,7 @@
 import rocketseatIcon from '@/assets/rocketseat-icon.svg';
+import { Slash } from 'lucide-react';
 import Image from 'next/image';
+import { OrganizationSwitcher } from './organization-switcher';
 import { ProfileButton } from './profile-button';
 
 export function Header() {
@@ -7,6 +9,10 @@ export function Header() {
     <div className='mx-auto flex max-w-[1200px] items-center justify-between'>
       <div className='flex items-center gap-3'>
         <Image src={rocketseatIcon} alt='Rocketseat' className='size-6 dark:invert' />
+
+        <Slash className='size-3 -rotate-[24deg] text-border' />
+
+        <OrganizationSwitcher />
       </div>
       <div className='flex items-center gap-4'>
         <ProfileButton />
